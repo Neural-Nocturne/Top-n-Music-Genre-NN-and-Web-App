38124c0c1129b1166308ba8942337f93a938ea4f
VFB Media ML <vfbmediaml@VFBs-MacBook-Pro-2.local>
2024-08-17 20:27:33 -0400

    fixed files staying behind in directories and not being deleted

--- src/web-app/app_controller.py
+++ src/web-app/app_controller.py
@@ -2,6 +2,7 @@ import model.input_checker
 import model.crop_files
 import model.format_results
 import model.predict
+import os
 
 
 def controller(form_data, options, filename=None):
@@ -12,9 +13,9 @@ def controller(form_data, options, filename=None):
     form_data: user submitted file in a dictionary, with key values Audio
     File or Youtube Link and containing the respective values in each"""
     if options == 1:
-        formated_file, title, unedited_title = model.input_checker.input_checker_and_formatter(form_data, form_data.content_type[-3:]) # noqa
+        formated_file, title, unedited_title, out_name = model.input_checker.input_checker_and_formatter(form_data, form_data.content_type[-3:]) # noqa
     else:
-        formated_file, title, unedited_title = model.input_checker.input_checker_and_formatter(form_data['Youtube Link']) # noqa
+        formated_file, title, unedited_title, out_name = model.input_checker.input_checker_and_formatter(form_data['Youtube Link']) # noqa
 
     if formated_file is None and title is None and unedited_title is None:
         return None, None
@@ -22,8 +23,10 @@ def controller(form_data, options, filename=None):
     sample1 = model.crop_files.crop_file_to_30_sec(formated_file, title, filename)
     if options == 1:
         results = model.predict.predict_music_genre('upload_folder/' + sample1) # noqa
+        os.remove(title + '.wav')
     else:
         results = model.predict.predict_music_genre(sample1)
+        os.remove(out_name + '.wav')
         print(f"Here are the results: {results}")
     results_dict = model.format_results.format_results(results[0])
     return results_dict, unedited_title
